410c473f04f88545d888989c5a6c628a96bc3a4a
kingwell47 <joeldoctor47@gmail.com>
2023-05-07 20:18:02 +0800

    added dashboard data

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,92 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
+import {
+  Box,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+import axios from "axios";
 
 const Dashboard = () => {
-  const authToken = localStorage.getItem("authToken");
+  const [schedule, setSchedule] = useState({});
+  const { user } = useSelector((state) => state.user);
 
-  if (!authToken) {
-    return <Navigate to="/login" replace />;
-  }
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+
+    if (!authToken) {
+      return <Navigate to="/login" replace />;
+    }
+
+    const fetchSchedule = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/employees/me/schedule",
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          }
+        );
+        setSchedule(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSchedule();
+  }, []);
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   return (
     <>
-      <div>Dashboard</div>
+      <Box display="flex" flexDirection="column" alignItems="center" p={2}>
+        <Typography variant="h5" align="center" gutterBottom>
+          Schedule
+        </Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Day</TableCell>
+              <TableCell>In</TableCell>
+              <TableCell>Out</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {days.map((day) => (
+              <TableRow key={day}>
+                <TableCell>{day}</TableCell>
+                {schedule[day] ? (
+                  <>
+                    <TableCell>{schedule[day].In}</TableCell>
+                    <TableCell>{schedule[day].Out}</TableCell>
+                  </>
+                ) : (
+                  <>
+                    <TableCell>-</TableCell>
+                    <TableCell>-</TableCell>
+                  </>
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
       <Outlet />
     </>
   );
